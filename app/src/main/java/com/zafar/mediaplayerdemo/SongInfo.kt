1ee89f1caa96929a1525471b173e78263fd0f95e
Zafar Imam <zafarimam512@gmail.com>
2020-04-22 13:51:43 +0530

    Initial commit

---
+++ app/src/main/java/com/zafar/mediaplayerdemo/SongInfo.kt
@@ -0,0 +1,16 @@
+package com.zafar.mediaplayerdemo
+
+
+
+class SongInfo{
+
+    var Title:String?=null
+    var AuthorName:String?=null
+    var SongURL:String?=null
+    constructor(Title:String,AuthorName:String,SongURL:String){
+        this.Title=Title
+        this.AuthorName=AuthorName
+        this.SongURL=SongURL
+    }
+
+}
